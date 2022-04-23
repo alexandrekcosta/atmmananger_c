df8cc85055a40345490dab938889c1a4b9d9bc0a
Alexandre Costa <68577176+alexandrekcosta@users.noreply.github.com>
2022-04-23 11:51:54 -0300

    Add files via upload
    A new C code has been added to shelf.

---
+++ Const/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define w 7
+
+int main()
+{
+
+    printf("%i\n",w);
+
+    const int c = 12;
+
+    printf("%i\n",c);
+    printf("%i\n",w+c);
+
+    return 0;
+}
